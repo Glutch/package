716b6ea4b48bdfcd84551bca683660d673c5456d
Tjoffex <tjoffex@gmail.com>
2017-11-14 16:56:09 +0100

    Update of order class, added database functionality

--- app/src/main/java/com/apackage/app/apackage/MainActivity.java
+++ app/src/main/java/com/apackage/app/apackage/MainActivity.java
@@ -3,12 +3,24 @@ package com.apackage.app.apackage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.apackage.app.apackage.database.DBHelper;
+
 public class MainActivity extends AppCompatActivity {
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //ghhghg
+
+    //testingDB();
+
+  }
+
+  //en metod för att testa databasfunktionalitet, ska ej vara kvar i slutprodukt
+  public void testingDB(){
+      DBHelper dbHelper = new DBHelper(this);
+      dbHelper.addOrder(111,"Ett namn", "En adress", 222, "En postort", 333, 444, "en tid", false);
+      dbHelper.getAllOrders();
+
   }
 }
