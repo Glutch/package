cb54ff802a9fdbf89846614bd1ca7aa2c99d68c7
mrx <tobjo057@student.liu.se>
2017-11-19 19:36:20 +0100

    Toolbar in place without buttons

--- app/src/main/java/com/apackage/app/apackage/viewOrders.java
+++ app/src/main/java/com/apackage/app/apackage/viewOrders.java
@@ -2,6 +2,7 @@ package com.apackage.app.apackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class viewOrders extends AppCompatActivity {
   
@@ -9,5 +10,8 @@ public class viewOrders extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_view_orders);
+
+    Toolbar toolbar = findViewById(R.id.toolbar2);
+    setSupportActionBar(toolbar);
   }
 }
