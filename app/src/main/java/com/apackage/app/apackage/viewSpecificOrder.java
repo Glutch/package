e4b40aceb680daca2ec42901f7e1ec77a08bfc6d
Kivanc <kivanc_wow424@live.se>
2017-11-23 19:53:10 +0100

    La till onMapready tillfället så lägger den marker i sydney Added funktion that ask permission for the device location

--- app/src/main/java/com/apackage/app/apackage/viewSpecificOrder.java
+++ app/src/main/java/com/apackage/app/apackage/viewSpecificOrder.java
@@ -1,7 +1,14 @@
 package com.apackage.app.apackage;
 
+import android.*;
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +16,17 @@ import android.widget.TextView;
 
 import com.apackage.app.apackage.database.DBHelper;
 import com.apackage.app.apackage.database.Order;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class viewSpecificOrder extends AppCompatActivity {
+public class viewSpecificOrder extends AppCompatActivity implements OnMapReadyCallback{
 
   private TextView clientID;
   private TextView ordernumber;
@@ -26,12 +42,19 @@ public class viewSpecificOrder extends AppCompatActivity {
   private Order order;
   private DBHelper dbHelper;
   private Button deliveredBtn;
+  private FusedLocationProviderClient mFusedLocationClient;
+  public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_view_specific_order);
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
+
+    mFusedLocationClient =  LocationServices.getFusedLocationProviderClient(this);
 
     Intent intent = getIntent();
     long id = intent.getLongExtra("ID", -1);
@@ -72,4 +95,78 @@ public class viewSpecificOrder extends AppCompatActivity {
     setResult(RESULT_OK , intent);
     finish();
   }
+
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    LatLng sydney = new LatLng(-33.852, 151.211);
+    googleMap.addMarker(new MarkerOptions().position(sydney)
+            .title("Marker in Sydney"));
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+  }
+
+  public boolean checkLocationPermission() {
+    if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+              Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+      } else {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                MY_PERMISSIONS_REQUEST_LOCATION);
+      }
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         String permissions[], int[] grantResults) {
+    switch (requestCode) {
+      case MY_PERMISSIONS_REQUEST_LOCATION: {
+
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+          if (ContextCompat.checkSelfPermission(this,
+                  Manifest.permission.ACCESS_FINE_LOCATION)
+                  == PackageManager.PERMISSION_GRANTED) {
+          }
+
+        } else {
+
+        }
+        return;
+      }
+
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (checkLocationPermission()) {
+      if (ContextCompat.checkSelfPermission(this,
+              Manifest.permission.ACCESS_FINE_LOCATION)
+              == PackageManager.PERMISSION_GRANTED) {
+
+      }
+    }
+
+  }
+
+
+
+
 }
