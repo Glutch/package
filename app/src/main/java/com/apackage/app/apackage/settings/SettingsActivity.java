73cb93b3e1367414f46e898b7597f6509c65adfd
mrx <tobjo057@student.liu.se>
2017-12-06 17:05:16 +0100

    Added toolbar to settings

--- app/src/main/java/com/apackage/app/apackage/settings/SettingsActivity.java
+++ app/src/main/java/com/apackage/app/apackage/settings/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.apackage.app.apackage.settings;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.apackage.app.apackage.R;
 
@@ -17,10 +18,14 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .add(R.id.frameLayout, new SettingsFragment())
                 .commit();
     }
 
